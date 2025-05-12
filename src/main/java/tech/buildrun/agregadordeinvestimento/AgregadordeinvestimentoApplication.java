c68fb3ad52ca6b4f591f86b2c58e692eb19dc23f
Luiza Goes <luizagoes087@gmail.com>
2025-05-12 13:19:54 -0300

    Commit inicial

---
+++ src/main/java/tech/buildrun/agregadordeinvestimento/AgregadordeinvestimentoApplication.java
@@ -0,0 +1,13 @@
+package tech.buildrun.agregadordeinvestimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadordeinvestimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregadordeinvestimentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/buildrun/agregadordeinvestimento/AgregadordeinvestimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.buildrun.agregadordeinvestimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadordeinvestimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
